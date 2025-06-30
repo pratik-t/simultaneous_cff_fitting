eaded96c002d82ef5978e81c0a6a9473ff0916e8
MidnightExcursion <dima.watkins@gmail.com>
2025-06-30 12:36:17 -0500

    major updates to simultaneous fitting routine

--- models/architecture.py
+++ models/architecture.py
@@ -6,6 +6,9 @@ any fitting procedure.
 # 3rd Party Library | TensorFlow:
 import tensorflow as tf
 
+# 3rd Party Library | bkm10:
+from bkm10_lib import DifferentialCrossSection, CFFInputs, BKM10Inputs, backend
+
 # 3rd Party Library | TensorFlow:
 from tensorflow.keras.layers import Input, Concatenate, Dense
 
@@ -23,6 +26,104 @@ from statics.static_strings import _HYPERPARAMETER_NUMBER_OF_NEURONS_LAYER_5
 
 SETTING_VERBOSE = True
 SETTING_DEBUG = True
+
+class CrossSectionLayer(tf.keras.layers.Layer):
+
+    def __init__(
+            self,
+            target_polarization = 0.0,
+            lepton_beam_polarization = 0.0,
+            using_ww = True,
+            **kwargs):
+        
+        # (1): Inherit Layer class properties:
+        super().__init__(**kwargs)
+
+        # (2): Obtain the target polarization:
+        self.target_polarization = target_polarization
+
+        # (3): Obtain the beam polarization:
+        self.lepton_beam_polarization = lepton_beam_polarization
+
+        # (4): Decide if we're using the WW relations:
+        self.using_ww = using_ww
+
+    def call(self, inputs):
+        """
+        ## Description:
+        This function is *required* in order to tell TF what to do when we register this
+        as a layer of some ANN. It should contain all the logic that is needed. Our goal here 
+        is to anticipate the passage of 5 + 4 different values --- in order, they are: lepton
+        beam energy, photon virtuality, Bjorken x, hadronic momentum transfer, the azimuthal 
+        angle phi, and the four CFFs. With these 9 inputs, we *compute* a single output that 
+        we call the cross section.
+        """
+
+        # (X): Unpack the inputs into the CFFs and the kinematics.
+        # | The inputs will be a KerasTensor of shape (None, 5) and another
+        # | KerasTensor of shape (None, 8). That is, the five kinematic
+        # | quantities and the eight numbers for the CFFs.
+        kinematics, cffs = inputs
+
+        # (X): Extract the eight CFFs from the DNN:
+        real_H, imag_H, real_E, imag_E, real_Ht, imag_Ht, real_Et, imag_Et = tf.unstack(cffs, axis = -1)
+
+        # (X): Extract the kinematics from the DNN:
+        q_squared, x_bjorken, t, k, phi = tf.unstack(kinematics, axis = -1)
+
+        # (X): DUMMY COMPUTATION FOR NOW:
+        # differential_cross_section = real_H**2 + imag_H**2 + 0.5 * tf.cos(phi) * real_E + 0.1 * q_squared
+
+        # (X): The calculation requires that we use TF not NumPy to do stuff:
+        backend.set_backend('tensorflow')
+
+        # (X): Set up the BKM10 kinematic inputs:
+        bkm_inputs = BKM10Inputs(
+            squared_Q_momentum_transfer = q_squared,
+            x_Bjorken = x_bjorken,
+            squared_hadronic_momentum_transfer_t = t,
+            lab_kinematics_k = k)
+
+        # (X): Set up the BKM10 CFF inputs:
+        cff_inputs = CFFInputs(
+            compton_form_factor_h = backend.math.complex(real_H, imag_H),
+            compton_form_factor_h_tilde = backend.math.complex(real_Ht, imag_Ht),
+            compton_form_factor_e = backend.math.complex(real_E, imag_E),
+            compton_form_factor_e_tilde = backend.math.complex(real_Et, imag_Et))
+
+        # (X): Construct the required configuration dictionary:
+        configuration = {
+            "kinematics": bkm_inputs,
+            "cff_inputs": cff_inputs,
+            "target_polarization": self.target_polarization,
+            "lepton_beam_polarization": self.lepton_beam_polarization,
+            "using_ww": self.using_ww
+        }
+
+        # (X): Compute the differential cross section accordingly:
+        differential_cross_section = DifferentialCrossSection(configuration).compute_cross_section(phi)
+
+        # (X): Re-cast sigma into a single value (I think):
+        return tf.expand_dims(differential_cross_section, axis = -1)
+    
+class BSALayer(tf.keras.layers.Layer):
+
+    def call(self, inputs):
+
+        # (X): Unpack the inputs into the CFFs and the kinematics:
+        kinematics, cffs = inputs
+
+        # (X): Extract the eight CFFs from the DNN:
+        real_H, imag_H, real_E, imag_E, real_Ht, imag_Ht, real_Et, imag_Et = tf.unstack(cffs, axis = -1)
+
+        # (X): Extract the kinematics from the DNN:
+        q_squared, x_bjorken, t, phi = tf.unstack(kinematics, axis = -1)
+
+        # (X): DUMMY COMPUTATION FOR NOW:
+        bsa = real_H**2 + imag_H**2 + 0.5 * tf.cos(phi) * real_E + 0.1 * q_squared
+
+        # (X): Re-cast the BSA into a single value (I think):
+        return tf.expand_dims(bsa, axis = -1)
 
 class SimultaneousFitModel(tf.keras.Model):
 
@@ -73,7 +174,7 @@ def build_simultaneous_model():
         seed = None)
     
     # (X): Define the input to the DNN:
-    input_kinematics = Input(shape=(5 ,), name = "input_layer")
+    input_kinematics = Input(shape = (5, ), name = "input_layer")
 
     # (X): Pass the inputs through a densely-connected hidden layer:
     x = Dense(
@@ -106,15 +207,15 @@ def build_simultaneous_model():
         kernel_initializer = initializer)(x)
     
     # (4): Combine the kinematics as a single list:
-    cff_predictions = Concatenate(axis = 1)([input_kinematics, output_cffs])
+    # kinematics_and_cffs = Concatenate(axis = 1)([input_kinematics, output_cffs])
 
     # (8): Compute, algorithmically, the cross section:
-    cross_section_value = CrossSectionLayer()(cff_predictions)
+    cross_section_value = CrossSectionLayer()([input_kinematics, output_cffs])
 
-    # (8): Compute, algorithmically, the BSA
-    bsa_value = BSALayer()(cff_predictions)
+    # (8): Compute, algorithmically, the BSA:
+    bsa_value = BSALayer()([input_kinematics, output_cffs])
 
-    # # (9): Define the model as as Keras Model:
+    # (9): Define the model as as Keras Model:
     simultaneous_fit_model = Model(
         inputs = input_kinematics,
         outputs = cross_section_value,
@@ -123,7 +224,7 @@ def build_simultaneous_model():
     if SETTING_DEBUG or SETTING_VERBOSE:
         print(simultaneous_fit_model.summary())
 
-    # (X):: Compile the model with a fixed learning rate using Adam and the custom loss:
+    # (X): Compile the model with a fixed learning rate using Adam and the custom loss:
     simultaneous_fit_model.compile(
         optimizer = tf.keras.optimizers.Adam(_HYPERPARAMETER_LEARNING_RATE),
         loss = tf.keras.losses.MeanSquaredError())
--- scripts/train_local_fit.py
+++ scripts/train_local_fit.py
@@ -12,11 +12,20 @@ import datetime
 # Native Library | os
 import os
 
+# 3rd Party Library | NumPy:
+import numpy as np
+
+# 3rd Party Library | Matplotlib:
+import matplotlib.pyplot as plt
+
 # 3rd Party Library | Pandas:
 import pandas as pd
 
 # 3rd Party Library | Pandas:
 import tensorflow as tf
+
+# 3rd Party Library | sklearn:
+from sklearn.model_selection import train_test_split
 
 from models.architecture import build_simultaneous_model
 
@@ -46,6 +55,12 @@ from statics.static_strings import _COLUMN_NAME_T_MOMENTUM_CHANGE
 # static_strings > "phi"
 from statics.static_strings import _COLUMN_NAME_AZIMUTHAL_PHI
 
+# (X):
+from statics.static_strings import _COLUMN_NAME_CROSS_SECTION
+
+# (X):
+from statics.static_strings import _COLUMN_NAME_CROSS_SECTION_ERROR
+
 from statics.static_strings import _HYPERPARAMETER_NUMBER_OF_EPOCHS
 
 from statics.static_strings import _HYPERPARAMETER_BATCH_SIZE
@@ -57,6 +72,26 @@ from statics.static_strings import _HYPERPARAMETER_LR_FACTOR
 from statics.static_strings import _HYPERPARAMETER_EARLYSTOP_PATIENCE_INTEGER
 
 from statics.static_strings import _DNN_VERBOSE_SETTING
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "text.latex.preamble": r"\usepackage{amsmath}"
+})
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['xtick.major.size'] = 5
+plt.rcParams['xtick.major.width'] = 0.5
+plt.rcParams['xtick.minor.size'] = 2.5
+plt.rcParams['xtick.minor.width'] = 0.5
+plt.rcParams['xtick.minor.visible'] = True
+plt.rcParams['xtick.top'] = True
+plt.rcParams['ytick.direction'] = 'in'
+plt.rcParams['ytick.major.size'] = 5
+plt.rcParams['ytick.major.width'] = 0.5
+plt.rcParams['ytick.minor.size'] = 2.5
+plt.rcParams['ytick.minor.width'] = 0.5
+plt.rcParams['ytick.minor.visible'] = True
+plt.rcParams['ytick.right'] = True
 
 SETTING_VERBOSE = True
 SETTING_DEBUG = True
@@ -108,16 +143,26 @@ def main(
         # (X): We now compute a *given* replica's DF --- it will *not* be the same as the original DF!
         generated_replica_data = generate_replica_data(pandas_dataframe = this_replica_data_set)
 
-        print(generated_replica_data)
-
         # (X): Identify the "x values" for our model:
-        raw_x_data = generated_replica_data[[_COLUMN_NAME_Q_SQUARED, _COLUMN_NAME_X_BJORKEN, _COLUMN_NAME_T_MOMENTUM_CHANGE, _COLUMN_NAME_LEPTON_MOMENTUM]]
+        raw_kinematics = generated_replica_data[[
+            _COLUMN_NAME_Q_SQUARED, 
+            _COLUMN_NAME_X_BJORKEN, 
+            _COLUMN_NAME_T_MOMENTUM_CHANGE,
+            _COLUMN_NAME_LEPTON_MOMENTUM,
+            _COLUMN_NAME_AZIMUTHAL_PHI]]
 
-        print(raw_x_data)
+        # (X): Obtain the cross section data from the replica dataframe:
+        raw_cross_section = generated_replica_data[_COLUMN_NAME_CROSS_SECTION]
 
-        # (X): Identify the "y values" for our model:
-        # raw_y_data = generated_replica_data[_COLUMN_NAME_CROSS_SECTION]
-        # yraw__data_error = generated_replica_data[_COLUMN_NAME_CROSS_SECTION_ERROR]
+        # (X): Obtain the associated cross section error from the replica dataframe:
+        raw_cross_section_error = generated_replica_data[_COLUMN_NAME_CROSS_SECTION_ERROR]
+
+        # (X): Use sklearn's traing/validation split function to split into training and testing data:
+        x_training, x_validation, y_training, y_validation = train_test_split(
+            raw_kinematics,
+            raw_cross_section,
+            test_size = 0.2,
+            random_state = 42)
 
         # (X): Begin timing the replica time:
         start_time_in_milliseconds = datetime.datetime.now().replace(microsecond = 0)
@@ -127,12 +172,26 @@ def main(
 
         # (X): Initialize the model:
         dnn_model = build_simultaneous_model()
-
+        
+        # (X): Here, we run the fitting procedure:
         neural_network_training_history = dnn_model.fit(
-            # training_x_data,
-            # training_y_data,
-            # validation_data = (testing_x_data, testing_y_data),
+
+            # (X): Insert the training input-data here (independent variables):
+            x_training,
+
+            # (X): Insert the training output-data here (dependent variables):
+            y_training,
+
+            # (X): Insert a tuple of validation data according to (input, output):
+            validation_data = (x_validation, y_validation),
+
+            # (X): Hyperparameter: Epoch number:
             epochs = _HYPERPARAMETER_NUMBER_OF_EPOCHS,
+
+            # (X): Hyperparameters: Batch size:
+            batch_size = _HYPERPARAMETER_BATCH_SIZE,
+
+            # (X): A list of TF callbacks:
             callbacks = [
                 tf.keras.callbacks.ReduceLROnPlateau(
                     monitor = 'loss',
@@ -143,11 +202,54 @@ def main(
                     monitor = 'loss',
                     patience = _HYPERPARAMETER_EARLYSTOP_PATIENCE_INTEGER)
             ],
-            batch_size = _HYPERPARAMETER_BATCH_SIZE,
-            verbose = _DNN_VERBOSE_SETTING)
 
+            # (X): TF verbose setting:
+            verbose = _DNN_VERBOSE_SETTING)
+        
         if SETTING_DEBUG or SETTING_VERBOSE:
             print(f"> Replica #{replica_index + 1} finished running!")
+
+        # (X): Extract the 'loss' key from TF's history object. It has 
+        # | loss vs. epoch data on it:
+        training_loss_data = neural_network_training_history.history['loss']
+
+        # (X): Extract the 'val_loss' (validation loss) from the TF history object:
+        validation_loss_history_array = neural_network_training_history.history['val_loss']
+            
+        evaluation_figure = plt.figure(
+            figsize = (11, 8))
+        
+        evaluation_axis = evaluation_figure.add_subplot(1, 1, 1)
+        
+        evaluation_axis.plot(
+            np.arange(0, _HYPERPARAMETER_NUMBER_OF_EPOCHS, 1),
+            np.array([np.max(training_loss_data) for number in training_loss_data]),
+            color = "red",
+            label = "Initial MSE Loss")
+        
+        evaluation_axis.plot(
+            np.arange(0, _HYPERPARAMETER_NUMBER_OF_EPOCHS, 1),
+            np.zeros(shape = _HYPERPARAMETER_NUMBER_OF_EPOCHS),
+            color = "green",
+            label = r"MSE $=0$")
+        
+        evaluation_axis.plot(
+            np.arange(0, _HYPERPARAMETER_NUMBER_OF_EPOCHS, 1),
+            training_loss_data,
+            color = "blue",
+            label = "MSE Loss")
+        
+        evaluation_axis.plot(
+            np.arange(0, _HYPERPARAMETER_NUMBER_OF_EPOCHS, 1),
+            validation_loss_history_array,
+            color = "purple",
+            label = "Validation Loss")
+        
+        evaluation_axis.set_xlabel('Epoch Number')
+        evaluation_axis.set_ylabel('Scalar Loss')
+        evaluation_axis.legend(fontsize = 20, shadow = True)
+        evaluation_figure.savefig(f"loss_analytics_replica_{replica_index + 1}_v1.png")
+        plt.close()
 
 
 if __name__ == "__main__":
